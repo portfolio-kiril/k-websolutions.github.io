6e34c3388c069a4a26e8efa62fcc5390017bb725
Kiril Zyapkov <ziapkov@gmail.com>
2022-06-18 13:49:54 +0300

    new design.

---
+++ projects.js
@@ -0,0 +1,38 @@
+export const Projects = [
+    {
+        _id: 0,
+        title: "Forum",
+        description: "A web site for finding everithing for cars or publish your own article. Joining diferent forums or create your own one.",
+        image: "assets/images/carForum.png",
+        tags:['Reacr', 'Back4App'],
+        liveDemo: "https://cars-forum.netlify.app/",
+        source: "https://github.com/KirilSZyapkov/blog-cars"
+    },
+    {
+        _id: 1,
+        title: "Picture Me",
+        description: "Social Media App using the following features, such as Google Authentication, create, edit, delete and save posts, like and comment on other people's posts, search and filter images.",
+        image: "assets/images/pictureMe.png",
+        tags:['Reacr', 'Back4App', 'Firebase Auth'],
+        liveDemo: "https://picture-me-react-project.netlify.app/",
+        source: "https://github.com/KirilSZyapkov/picture_me"
+    },
+    {
+        _id: 2,
+        title: "My Storage",
+        description: "A Goole Drive clone app using Firebase Authentication, Firebase storage and Database. You can create an accound and store your files and creating new folders.",
+        image: "assets/images/myStorage.png",
+        tags:['Reacr', 'Firebase Storage', 'Firebase Auth', 'Firebase Database'],
+        liveDemo: "",
+        source: "https://github.com/KirilSZyapkov/my-storage"
+    },
+    {
+        _id: 3,
+        title: "Chat App",
+        description: "A chat app that allows to communicate with your friends.",
+        image: "assets/images/chatApp.jpg",
+        tags:['Reacr', 'Chat Engine'],
+        liveDemo: "https://chat-app-uni.netlify.app/",
+        source: "https://github.com/KirilSZyapkov/chat-app"
+    },
+]
