8edf4e82838729f6f9a8d2c9b5f8d7130317324a
Kiril Zyapkov <ziapkov@gmail.com>
2023-01-03 22:04:25 +0200

    Add new project to portfolio section.

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ export const Projects = [
         title: "Forum",
         description: "A web site for finding everithing for cars or publish your own article. Joining diferent forums or create your own one.",
         image: "assets/images/carForum.png",
-        tags:['Reacr', 'Back4App'],
+        tags:['React', 'Back4App'],
         liveDemo: "https://cars-forum.netlify.app/",
         source: "https://github.com/KirilSZyapkov/blog-cars"
     },
@@ -13,7 +13,7 @@ export const Projects = [
         title: "Picture Me",
         description: "Social Media App using the following features, such as Google Authentication, create, edit, delete and save posts, like and comment on other people's posts, search and filter images.",
         image: "assets/images/pictureMe.png",
-        tags:['Reacr', 'Back4App', 'Firebase Auth'],
+        tags:['React', 'Back4App', 'Firebase Auth'],
         liveDemo: "https://picture-me-react-project.netlify.app/",
         source: "https://github.com/KirilSZyapkov/picture_me"
     },
@@ -22,7 +22,7 @@ export const Projects = [
         title: "My Storage",
         description: "A Goole Drive clone app using Firebase Authentication, Firebase storage and Database. You can create an accound and store your files and creating new folders.",
         image: "assets/images/myStorage.png",
-        tags:['Reacr', 'Firebase Storage', 'Firebase Auth', 'Firebase Database'],
+        tags:['React', 'Firebase Storage', 'Firebase Auth', 'Firebase Database'],
         liveDemo: "",
         source: "https://github.com/KirilSZyapkov/my-storage"
     },
@@ -31,8 +31,17 @@ export const Projects = [
         title: "Chat App",
         description: "A chat app that allows to communicate with your friends.",
         image: "assets/images/chatApp.jpg",
-        tags:['Reacr', 'Chat Engine'],
+        tags:['React', 'Chat Engine'],
         liveDemo: "https://chat-app-uni.netlify.app/",
         source: "https://github.com/KirilSZyapkov/chat-app"
     },
+    {
+        _id: 4,
+        title: "Open Chat AI",
+        description: "An AI GPT chat app using the latest OpenAI technology.",
+        image: "assets/images/open_chat_ai.jpg",
+        tags:['Vnila JS', 'Express', 'OpenAI'],
+        liveDemo: "https://open-chat-ai.netlify.app/",
+        source: "https://github.com/KirilSZyapkov/open_chat_ai"
+    }
 ]
